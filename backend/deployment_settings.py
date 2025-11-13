c364cfcdb3c80c4effeba8c2a70763db88916bd4
bkam-dev <angemichaelboni10@gmail.com>
2025-11-13 01:19:51 +0100

    Fix: deployments settings of MickTodo

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -1,55 +1,59 @@
 import os
 import dj_database_url
 from .settings import *
-from .settings import BASE_DIR
-from decouple import config, Csv
+from decouple import config, Csv 
 
 
-RENDER_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_HOST = os.environ.get('RENDER_EXTERNAL_HOSTNAME') 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY') # Utiliser os.environ.get
+
+DEBUG = False 
+
+ALLOWED_HOSTS = [RENDER_HOST] if RENDER_HOST else [] 
+
+if RENDER_HOST:
+    CSRF_TRUSTED_ORIGINS = ['https://' + RENDER_HOST]
+    CSRF_TRUSTED_ORIGINS.append('https://micktodo-frontend.vercel.app')
+else:
+    CSRF_TRUSTED_ORIGINS = []
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
-
-ALLOWED_HOSTS = RENDER_HOSTNAME
-CSRF_TRUSTED_ORIGINS = ['https://'+ RENDER_HOSTNAME]
-
-DEBUG = False
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]  
+]
 
 
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS')
+cors_origins_raw = os.environ.get('CORS_ALLOWED_ORIGINS', '')
+CORS_ALLOWED_ORIGINS = cors_origins_raw.split(',') if cors_origins_raw else []
+
 
 STORAGES = {
     "default":{
         "BACKEND" : "django.core.files.storage.FileSystemStorage"
     },
     "staticfiles":{
-        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage"
+        "BACKEND" : "whitenoise.storage.CompressedManifestStaticFilesStorage" 
     },
 }
 
-
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ['DATABASE_URL'],
+        default=DATABASE_URL,
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=True 
     )
 }
-
-
-# SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT')
-# SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE')
-# CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE')
